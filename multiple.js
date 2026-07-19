4eb814383b9e87643ab3a3658c1527e2e6d32e51
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 02:09:42 +0600

    class 5 done

--- multiple.js
+++ multiple.js
@@ -3,13 +3,24 @@
  *  
  * */
 
-const salary=250000;
-const isBCS=true;
+const salary=40000;
+const isBCS=false;
 const hasCar=true;
 
-if(salary>=50000){
-    console.log('Biye dibo')
+// if(salary>=50000 && isBCS===true && hasCar===true){
+//     // console.log('Biye dibo')
+// }
+// else{
+//     console.log('Biye dibo na')
+// }
+
+// if(salary>50000 || isBCS===true){
+//     console.log('Biye dibo')
+// }
+
+if(salary>=50000 && hasCar===true || isBCS===true){
+    console.log('Biya Dimu')
 }
 else{
-    console.log('Biye dibo na')
+    console.log('Ja Vag')
 }
