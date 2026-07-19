f4f3538294ea9ddd95510d5c3be577234ad710cb
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 04:11:55 +0600

    not.js

---
+++ not.js
@@ -0,0 +1,11 @@
+const isLoggedIn =true;
+// eijaygay jodi isLoggedIn er samne ! deya hoy tail seitar ortho hobe not. mane ultai jabe , true thakle false ar false thakle true hbe .
+// if(isLoggedIn === true){  
+//     console.log('Add Your Comment')
+// }
+// else{
+//     console.log('Please Login First')
+// }
+
+// Upper system of this//
+// ---------------------//
