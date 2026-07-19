da7c3a1b0a24358140ce9353f379d5ce27d4dedb
tanvir134338 <tanvir134338@gmail.com>
2026-07-20 01:16:04 +0600

    folder changed

---
+++ multiple.js
@@ -0,0 +1,15 @@
+/**
+ * MULTIPLE CONDITIONS
+ *  
+ * */
+
+const salary=250000;
+const isBCS=true;
+const hasCar=true;
+
+if(salary>=50000){
+    console.log('Biye dibo')
+}
+else{
+    console.log('Biye dibo na')
+}
